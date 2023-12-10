75a1a4f2434abdf1d58aaa0c7e886926019deba7
Mathias Roesler <mroe734@bn418243.uoa.auckland.ac.nz>
2023-12-11 09:24:46 +1300

    Wrote the script to compare the pregnant and non-pregnant models with the L2 norm

---
+++ code/PNP-comp.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# PNP-comp.py: Compares the results from pregnant and non-pregnant models
+# Author: Mathias Roesler
+# Last modified: 12/23
+
+import os
+import sys
+import argparse
+import pickle
+import numpy as np
+import Roesler2024
+import Means2022
+import functions
+
+if __name__ == "__main__":
+	init_states_M, constants_M = Means2022.initConsts()
+	init_states_R, constants_R = Roesler2024.initConsts()
+	_, _, _, legend_constants_R = Roesler2024.createLegends()
+
+	print("Computing Means2022 simulation")
+	voi_M, states_M, _ = Means2022.solveModel(init_states_M, constants_M)
+	l2_points = np.zeros(4) # L2 points for each stage of the estrus
+
+	for i, key in enumerate(functions.ESTRUS.keys()):
+		# Set estrus dependant constants
+		constants_R = functions.setEstrusParams(constants_R, 
+			legend_constants_R, key)
+
+		print("Computing Roesler2024 {} simulation".format(key))
+
+		voi_R, states_R, _ = Roesler2024.solveModel(init_states_R, constants_R)
+		l2_points[i] = functions.computeL2Norm(states_M, states_R)
+
