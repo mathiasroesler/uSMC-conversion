39f32cac13275f262865f8d4d45b2b84e47da1a2
Mathias Roesler <mroe734@bn418243.uoa.auckland.ac.nz>
2023-12-11 17:22:35 +1300

    Added an optional flag to just plot results and not recompute everything

--- code/PNP-comp.py
+++ code/PNP-comp.py
@@ -21,40 +21,44 @@ if __name__ == "__main__":
 
 	parser.add_argument("metric", type=str, choices={"l2", "rmse", "mae"}, 
 		help="comparison metric")
+	parser.add_argument("-p", "--plot-only", action="store_true", 
+		help="flag used just to plot data")
 
 	args = parser.parse_args()
 	init_states_M, constants_M = Means2022.initConsts()
 	init_states_R, constants_R = Roesler2024.initConsts()
 	_, _, _, legend_constants_R = Roesler2024.createLegends()
 
-	print("Computing Means2022 simulation")
-	voi_M, states_M, _ = Means2022.solveModel(init_states_M, constants_M)
-	comp_points = np.zeros(4) # Comparison points for each stage of the estrus
+	if not args.plot_only:
+		print("Computing Means2022 simulation")
+		voi_M, states_M, _ = Means2022.solveModel(init_states_M, constants_M)
+		comp_points = np.zeros(4) # Comparison points for each stage of estrus
 
-	for i, key in enumerate(functions.ESTRUS.keys()):
-		# Set estrus dependant constants
-		constants_R = functions.setEstrusParams(constants_R, 
-			legend_constants_R, key)
+		for i, key in enumerate(functions.ESTRUS.keys()):
+			# Set estrus dependant constants
+			constants_R = functions.setEstrusParams(constants_R, 
+				legend_constants_R, key)
 
-		print("Computing Roesler2024 {} simulation".format(key))
+			print("Computing Roesler2024 {} simulation".format(key))
 
-		voi_R, states_R, _ = Roesler2024.solveModel(init_states_R, constants_R)
+			voi_R, states_R, _ = Roesler2024.solveModel(init_states_R, 
+				constants_R)
 
-		match args.metric:
-			case "l2":
-				comp_points[i] = functions.computeL2Norm(states_M, states_R)
+			match args.metric:
+				case "l2":
+					comp_points[i] = functions.computeL2Norm(states_M, states_R)
 
-			case "rmse":
-				comp_points[i] = functions.computeRMSE(states_M, states_R)
+				case "rmse":
+					comp_points[i] = functions.computeRMSE(states_M, states_R)
 
-			case "mae":
-				comp_points[i] = functions.computeMAE(states_M, states_R)	
+				case "mae":
+					comp_points[i] = functions.computeMAE(states_M, states_R)	
 
-	output_file = "../res/{}_comp.pkl".format(args.metric)
+		output_file = "../res/{}_comp.pkl".format(args.metric)
 
-	with open(output_file, 'wb') as handler:
-		# Pickle data
-		pickle.dump(comp_points / max(comp_points), handler)
+		with open(output_file, 'wb') as handler:
+			# Pickle data
+			pickle.dump(comp_points / max(comp_points), handler)
 
 	# Plot normalized Euclidean distances
 	plots.plotPNPComp(args.metric)
