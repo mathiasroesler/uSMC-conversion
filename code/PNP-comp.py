a3f0590dd46f858fb48a077075a88b01b398bc9e
Mathias Roesler <mroe734@bn418243.uoa.auckland.ac.nz>
2023-12-11 17:04:30 +1300

    Updated the comparison script to be able to switch comparison metric

--- code/PNP-comp.py
+++ code/PNP-comp.py
@@ -7,6 +7,7 @@
 
 import os
 import sys
+import argparse
 import numpy as np
 import Roesler2024
 import Means2022
@@ -14,13 +15,20 @@ import functions
 import plots
 
 if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description=
+		"Compares the pregnant and non-pregnant models")
+
+	parser.add_argument("metric", type=str, choices={"l2", "rmse", "mae"}, 
+		help="comparison metric")
+
+	args = parser.parse_args()
 	init_states_M, constants_M = Means2022.initConsts()
 	init_states_R, constants_R = Roesler2024.initConsts()
 	_, _, _, legend_constants_R = Roesler2024.createLegends()
 
 	print("Computing Means2022 simulation")
 	voi_M, states_M, _ = Means2022.solveModel(init_states_M, constants_M)
-	l2_points = np.zeros(4) # L2 points for each stage of the estrus
+	comp_points = np.zeros(4) # Comparison points for each stage of the estrus
 
 	for i, key in enumerate(functions.ESTRUS.keys()):
 		# Set estrus dependant constants
@@ -30,7 +38,16 @@ if __name__ == "__main__":
 		print("Computing Roesler2024 {} simulation".format(key))
 
 		voi_R, states_R, _ = Roesler2024.solveModel(init_states_R, constants_R)
-		l2_points[i] = functions.computeL2Norm(states_M, states_R)
+
+		match args.metric:
+			case "l2":
+				comp_points[i] = functions.computeL2Norm(states_M, states_R)
+
+			case "rmse":
+				comp_points[i] = functions.computeRMSE(states_M, states_R)
+
+			case "mae":
+				comp_points[i] = functions.computeMAE(states_M, states_R)	
 
 	# Plot normalized Euclidean distances
-	plots.plotPNPComp(l2_points / max(l2_points))
+	plots.plotPNPComp(comp_points / max(comp_points))
