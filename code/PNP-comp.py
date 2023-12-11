dab5f28dcedc2d375c9ea1b85358a721988c191f
Mathias Roesler <mroe734@bn418243.uoa.auckland.ac.nz>
2023-12-12 10:03:30 +1300

    Updated the comparison script to plot the simulation outputs

--- code/PNP-comp.py
+++ code/PNP-comp.py
@@ -29,9 +29,12 @@ if __name__ == "__main__":
 	init_states_R, constants_R = Roesler2024.initConsts()
 	_, _, _, legend_constants_R = Roesler2024.createLegends()
 
+	sim_output = dict() # Store simulation output results
+	sim_file = "../res/sim_output.pkl"
+
 	if not args.plot_only:
 		print("Computing Means2022 simulation")
-		voi_M, states_M, _ = Means2022.solveModel(init_states_M, constants_M)
+		_, states_M, _ = Means2022.solveModel(init_states_M, constants_M)
 		comp_points = np.zeros(4) # Comparison points for each stage of estrus
 
 		for i, key in enumerate(functions.ESTRUS.keys()):
@@ -43,15 +46,26 @@ if __name__ == "__main__":
 
 			voi_R, states_R, _ = Roesler2024.solveModel(init_states_R, 
 				constants_R)
-			
+			sim_output[key] = states_R[0, :] # Membrane potential for plot
 			comp_points[i] = functions.computeComparison(
 				states_M, states_R, args.metric)
 
-		output_file = "../res/{}_comp.pkl".format(args.metric)
+		sim_output["time"] = voi_R / 1000 # Add timesteps in s
 
-		with open(output_file, 'wb') as handler:
+		comp_file = "../res/{}_comp.pkl".format(args.metric)
+
+		with open(comp_file, 'wb') as handler:
 			# Pickle data
 			pickle.dump(comp_points / max(comp_points), handler)
 
+		with open(sim_file, 'wb') as handler:
+			# Pickle data
+			pickle.dump(sim_output, handler)
+
+	else:
+		with open(sim_file, 'rb') as handler:
+			# Unpickle data
+			sim_output = pickle.load(handler)
+
 	# Plot normalized Euclidean distances
-	plots.plotPNPComp(args.metric)
+	plots.plotSimulationOutput(sim_output, args.metric)
